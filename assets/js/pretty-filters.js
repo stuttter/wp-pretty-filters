cde922faf4249b9acdc4e5433c07ebeaf99c0ac8
John James Jacoby <johnjamesjacoby@me.com>
2015-11-18 15:15:25 -0600

    First pass at pretty comment filters.

--- assets/js/pretty-filters.js
+++ assets/js/pretty-filters.js
@@ -2,12 +2,21 @@ jQuery( document ).ready( function( $ ) {
     'use strict';
 
 	// Get the filters
-	var toptablenav = $( '#posts-filter .tablenav.top' ),
-		filters     = toptablenav.find( '.actions:not(.bulkactions)' );
+	var toptablenav = $( '#posts-filter .tablenav.top' );
 
-	// Add a class for custom styling
-	filters.addClass( 'wp-pretty-filters' );
+	// Maybe try comments
+	if ( 'undefined' === typeof( toptablenav ) ) {
+		toptablenav = $( '#commentsform .tablenav.top' );
+	}
 
-	// Relocate
-	toptablenav.before( filters );
+	// Only proceed if toptablenav was found
+	if ( 'undefined' !== typeof( toptablenav ) ) {
+		var filters = toptablenav.find( '.actions:not(.bulkactions)' );
+
+		// Add a class for custom styling
+		filters.addClass( 'wp-pretty-filters' );
+
+		// Relocate
+		toptablenav.before( filters );
+	}
 } );
