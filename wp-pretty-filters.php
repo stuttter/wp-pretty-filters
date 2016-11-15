8cc0b974d73c72cff1236afbca52532e659ef360
John James Jacoby <johnjamesjacoby@me.com>
2016-11-15 09:45:32 -0600

    Update author URL.

--- wp-pretty-filters.php
+++ wp-pretty-filters.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Pretty Filters
  * Plugin URI:  http://wordpress.org/plugins/wp-pretty-filters/
  * Author:      John James Jacoby
- * Author URI:  https://jjj.me/
+ * Author URI:  https://jjj.blog/
  * License:     GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wp-pretty-filters
